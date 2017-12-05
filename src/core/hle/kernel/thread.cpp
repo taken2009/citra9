d413ac6f0e6d94e80dc7768d8aca893a4d278ece
taken2009 <32885332+taken2009@users.noreply.github.com>
2017-12-05 16:55:12 +0900

    Dragios's priority-boost

--- src/core/hle/kernel/thread.cpp
+++ src/core/hle/kernel/thread.cpp
@@ -98,7 +98,29 @@ void Thread::Stop() {
         ((tls_address - Memory::TLS_AREA_VADDR) % Memory::PAGE_SIZE) / Memory::TLS_ENTRY_SIZE;
     Kernel::g_current_process->tls_slots[tls_page].reset(tls_slot);
 }
+    
+/// Boost low priority threads (temporarily) that have been starved
+static void PriorityBoostStarvedThreads() {
+    u64 current_ticks = CoreTiming::GetTicks();
 
+    for (auto& thread : thread_list) {
+        // TODO(bunnei): Threads that have been waiting to be scheduled for `boost_ticks` (or
+        // longer) will have their priority temporarily adjusted to 1 higher than the highest
+        // priority thread to prevent thread starvation. This general behavior has been verified
+        // on hardware. However, this is almost certainly not perfect, and the real CTR OS scheduler
+        // should probably be reversed to verify this.
+
+        const u64 boost_timeout = 2000000; // Boost threads that have been ready for > this long
+
+        u64 delta = current_ticks - thread->last_running_ticks;
+
+        if (thread->status == THREADSTATUS_READY && delta > boost_timeout) {
+            const s32 priority = std::max(ready_queue.get_first()->current_priority - 1,
+                                 static_cast<unsigned int>(0));
+            thread->BoostPriority(priority);
+        }
+    }
+}
 /**
  * Switches the CPU's active thread context to that of the specified thread
  * @param new_thread The thread to switch to
@@ -133,7 +155,9 @@ static void SwitchContext(Thread* new_thread) {
 
         ready_queue.remove(new_thread->current_priority, new_thread);
         new_thread->status = THREADSTATUS_RUNNING;
-
+        
+        // Restores thread to its nominal priority if it has been temporarily changed
+        new_thread->current_priority = new_thread->nominal_priority;
         if (previous_process != current_thread->owner_process) {
             Kernel::g_current_process = current_thread->owner_process;
             SetCurrentPageTable(&Kernel::g_current_process->vm_manager.page_table);
@@ -465,6 +489,8 @@ bool HaveReadyThreads() {
 }
 
 void Reschedule() {
+    PriorityBoostStarvedThreads();
+
     Thread* cur = GetCurrentThread();
     Thread* next = PopNextReadyThread();
 
